ad83f81757141c29395f31f0f19f4cc0592e88e1
Rose Crisp <rocrisp@redhat.com>
2021-10-19 12:44:28 -0400

    setup operator

--- operatorframework/operator-admission-webhooks/set-env.sh
+++ operatorframework/operator-admission-webhooks/set-env.sh
@@ -6,7 +6,7 @@ curl https://raw.githubusercontent.com/golang/dep/master/install.sh | sh
 #Temporarily update operator sdk to v1.9 to fix initialization error
 wget https://github.com/operator-framework/operator-sdk/releases/download/v1.9.0/operator-sdk_linux_amd64
 chmod +x operator-sdk_linux_amd64
-mv operator-sdk-v1.6.2-x86_64-linux-gnu /root/tutorial/go/bin/operator-sdk -f
+mv operator-sdk_linux_amd64 /root/tutorial/go/bin/operator-sdk -f
 
 #setup Memcached-operator
 oc new-project myproject
